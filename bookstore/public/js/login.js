a2b81fe6889eeec8ed63a6f4034914fb83a19de6
Behlul41 <mbehlulcelik@gmail.com>
2024-06-07 19:53:02 +0300

    Top Secret Commit

--- bookstore/public/js/login.js
+++ bookstore/public/js/login.js
@@ -1,8 +1,11 @@
+console.log("Merhaba");
 $(document).ready(function(){
-    $("#login_form").on("submit", function(event){
+    $("#login-button").on("submit", function(event){
         event.preventDefault();
         let login_username = $("#login-user-name").val();
         let login_password = $("#login-password").val();
+        console.log("Oke");
+        console.log(login_password);
 
         $.ajax({
             url: "/pages/login.html",
